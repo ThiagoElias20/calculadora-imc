6aeaaf41124fb53db7c178ff0c92a033d1025468
Thiago Elias <contatothiagoelias22@gmail.com>
2023-01-12 14:14:11 -0300

    Voltando com projetos com javascript

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+function imc() {
+  let peso = document.getElementById('peso').value
+  let altura = document.getElementById('altura').value
+
+  let altura2 = altura * altura
+  let imc = peso / altura2
+
+  document.getElementById('res').innerText = `Seu IMC é ${imc}`
+
+  if (imc < 18.5 ) {
+    document.getElementById('tipo').innerText = `Você Possui: Magreza`
+   } else if (imc >= 18.5 && imc <= 24.9) {
+    document.getElementById('tipo').innerText = `Você Possui: Normal`
+   } else if (imc >= 25 && imc <= 29.9) {
+    document.getElementById('tipo').innerText = `Você Possui: Sobrepeso`
+   } else if (imc >= 30 && imc <= 39.9) {
+    document.getElementById('tipo').innerText = `Você Possui: Obesidade`
+   } else if (imc > 40) {
+    document.getElementById('tipo').innerText = `Você Possui: Obesidade grave`
+   }
+}
+
+
+// mutiplicamos a altura por si mesmo, depois dividimos altura por peso ao quadrado
