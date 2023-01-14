be50dfdac23900faf3aa6cb0d2a1f91229539035
Thiago Elias <contatothiagoelias22@gmail.com>
2023-01-14 16:13:58 -0300

    Finalizei calculo do IMC e a virgula automatica foi adicionada ao escrever na altura

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 
 
-function imc() {
-  let peso = document.getElementById('peso')
-  let altura = document.getElementById('altura')
+// function imc() {
+//   let peso = document.getElementById('peso')
+//   let altura = document.getElementById('altura')
 
-  let altura2 = altura.value * altura.value
-  let imc = Math.trunc(Number(peso.value) / Number(altura2))
+//   let altura2 = altura.value * altura.value
+//   let imc = Math.trunc(Number(peso.value) / Number(altura2))
 
-  document.getElementById('res').innerText = `${String(imc)}`
+//   document.getElementById('res').innerText = `${String(imc)}`
 
-  if (imc < 18.5 ) {
-    document.getElementById('tipo').innerText = `Você Possui: Magreza`
-   } else if (imc >= 18.5 && imc <= 24.9) {
-    document.getElementById('tipo').innerText = `Você Possui: Normal`
-   } else if (imc >= 25 && imc <= 29.9) {
-    document.getElementById('tipo').innerText = `Você Possui: Sobrepeso`
-   } else if (imc >= 30 && imc <= 39.9) {
-    document.getElementById('tipo').innerText = `Você Possui: Obesidade`
-   } else if (imc > 40) {
-    document.getElementById('tipo').innerText = `Você Possui: Obesidade grave`
-   }
-  }
+//   if (imc < 18.5 ) {
+//     document.getElementById('tipo').innerText = `Você Possui: Magreza`
+//    } else if (imc >= 18.5 && imc <= 24.9) {
+//     document.getElementById('tipo').innerText = `Você Possui: Normal`
+//    } else if (imc >= 25 && imc <= 29.9) {
+//     document.getElementById('tipo').innerText = `Você Possui: Sobrepeso`
+//    } else if (imc >= 30 && imc <= 39.9) {
+//     document.getElementById('tipo').innerText = `Você Possui: Obesidade`
+//    } else if (imc > 40) {
+//     document.getElementById('tipo').innerText = `Você Possui: Obesidade grave`
+//    }
+//   }
   
 // mutiplicamos a altura por si mesmo, depois dividimos altura por peso ao quadrado
 
@@ -48,10 +48,55 @@ miguel();
 */
 
 
-const input = document.getElementById('altura')
-const testando = () => {
-  if(input.value.length === 1)input.value = `${input.value},`
+function imc() {
+  let peso = document.getElementById('peso')
+  let altura = document.getElementById('altura')
+  let novaAltura = ''
+  peso = Number(peso.value)
+
+  for (let i = 0; i < altura.value.length; i++){
+      if(altura.value[i] === ',') novaAltura += '.'
+      else{
+          novaAltura += altura.value[i] // novaAltura = novaAltura + altura.value[i] 
+      }
+      console.log(novaAltura);
+  }
+
+  let altura2 = Number(novaAltura * novaAltura)
+  let imc = String((peso / altura2).toFixed(2)) 
+
+  let novoIMC = ''
+  for (let i = 0; i < imc.length; i++){
+      if(imc[i] === '.') novoIMC += ','
+      else{
+          novoIMC += imc[i]
+      }
+  console.log(novoIMC);
+  }
+
+
+  document.getElementById('res').innerText = `${novoIMC}`
+
+  if (imc < 18.5 ) {
+    document.getElementById('tipo').innerText = `Você Possui: Magreza`
+   } else if (imc >= 18.5 && imc <= 24.9) {
+    document.getElementById('tipo').innerText = `Você Possui: Normal`
+   } else if (imc >= 25 && imc <= 29.9) {
+    document.getElementById('tipo').innerText = `Você Possui: Sobrepeso`
+   } else if (imc >= 30 && imc <= 39.9) {
+    document.getElementById('tipo').innerText = `Você Possui: Obesidade`
+   } else if (imc > 40) {
+    document.getElementById('tipo').innerText = `Você Possui: Obesidade grave`
+   }
+  }
+  
+
+function testando(){
+  if(altura.value.length === 1)altura.value = `${altura.value},`
 }
-input.addEventListener('keydown',(e) => {
-  if(e.keyCode !== 8) testando()
+  
+altura.addEventListener('keydown',(e) => {
+  if(e.keyCode !== 8){
+      testando()
+  } 
 })
